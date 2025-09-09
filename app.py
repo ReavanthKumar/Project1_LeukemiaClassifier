1eaac0ac44a20a9d595d5ab1276e9e72f1bdf6d6
R Reavanth Kumar <reavanth2003@gmail.com>
2025-09-10 02:34:04 +0530

    added changes

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Load ResNet34
 resnet34 = models.resnet34(weights=models.ResNet34_Weights.IMAGENET1K_V1)
 resnet34.fc = nn.Linear(resnet34.fc.in_features, num_classes)
-resnet34.load_state_dict(torch.load("best_model_resnet34.pth", map_location=device))
+resnet34.load_state_dict(torch.load("models/best_model_resnet34.pth", map_location=device))
 resnet34.eval().to(device)
 
 # Load Inception-v3
@@ -47,7 +47,7 @@ if inception.AuxLogits is not None:
     inception.AuxLogits.fc = nn.Linear(inception.AuxLogits.fc.in_features, num_classes)
 
 # Load trained weights
-state_dict = torch.load("best_model_inceptionv3.pth", map_location=device)
+state_dict = torch.load("models/best_model_inceptionv3.pth", map_location=device)
 inception.load_state_dict(state_dict)
 
 inception = inception.to(device)
